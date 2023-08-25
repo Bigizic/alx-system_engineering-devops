6022203345bf94203e4f3e3d14f3fcbc37231d81
Bigizic <olalekanisaac75@gmail.com>
2023-08-25 14:30:14 +0000

    update_puppet_files

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,10 +1,6 @@
 # a pp script that kills a process named killmenow
-class kill_me_now {
 exec { 'kill_process':
 command => 'pkill -f killmenow',
 onlyif  => 'pgrep -f killmenow',
 path    => '/usr/bin',
 }
-}
-
-include kill_me_now
