0c08e1e736210662d39d1958ded520bd06454bf8
Bigizic <olalekanisaac75@gmail.com>
2023-08-26 17:18:49 +0000

    Update puppet_ssh_config.pp

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,9 +1,10 @@
 #puppet code representation of the 2-ssh_config file
 
-exec {'/.ssh/config':
+file {'/.ssh/config':
 ensure  => 'file',
 content => "
-Host 54.82.132.33
+Host server297651-web-01
+HostName 54.82.132.33
 User ubuntu
 IdentityFile ~/.ssh/school
 PasswordAuthentication no
