45573a39701d28e0a8eb99023922ed7cd20d06a6
Bigizic <olalekanisaac75@gmail.com>
2023-09-05 01:11:41 +0000

    Update puppet_custom_http_response_header.pp

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -15,33 +15,8 @@ content => 'Hello World!',
 }
 
 # Add a custom HTTP header response
-file { '/etc/nginx/sites-available/custom-header':
-ensure  => present,
-content => '
-server {
-listen 80 default_server;
-server_name _;
-
-location / {
-add_header X-Custom-Header "This is a custom header";
-root   /var/www/html;
-index  some_page.html;
-}
-}
-',
-require => Package['nginx'],
-}
-
-# Enable the custom header site
-file { '/etc/nginx/sites-enabled/custom-header':
-ensure  => link,
-target  => '/etc/nginx/sites-available/custom-header',
-require => File['/etc/nginx/sites-available/custom-header'],
-}
-
-# Ensure Nginx service is running and reload configuration when necessary
-service { 'nginx':
-ensure     => running,
-hasrestart => true,
-require    => [Package['nginx'], File['/etc/nginx/sites-available/custom-header']],
+file { '/etc/nginx/sites-available/default':
+ensure => present,
+after  => 'listen 80 default_server;',
+line   => 'add_header X-Served-By $hostname;',
 }
