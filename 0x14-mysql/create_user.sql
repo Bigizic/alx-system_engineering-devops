485d30bf615e8127f299136f443d95deef7dd325
Bigizic <olalekanisaac75@gmail.com>
2023-09-19 19:42:33 +0000

    Add create_user.sql

--- 0x14-mysql/create_user.sql
+++ 0x14-mysql/create_user.sql
@@ -1,4 +1,4 @@
 -- my sql syntax to create the user and grant permission
--- to use cat create_user.sql | mysql -uroot -p
+-- to use "cat create_user.sql | mysql -uroot -p"
 CREATE USER IF NOT EXISTS 'holberton_user'@'localhost' IDENTIFIED BY '';
 GRANT REPLICATION CLIENT ON *.* TO 'holberton_user'@'localhost';
