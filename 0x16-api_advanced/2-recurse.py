e1edb0d7b02855098272fd019b5067d09ba4e132
Bigiizc <olalekanisaac75@gmail.com>
2023-10-10 20:20:34 +0100

    Update recurse.py

---
+++ 0x16-api_advanced/2-recurse.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""A python module that query the reddit api recursively
+
+Return:
+    a list containing titles of all hot articles
+    otherwise None for invalid subreddit
+"""
+
+
+import requests
+
+
+def recurse(subreddit, hot_list=[], i=0):
+    url = f"https://www.reddit.com/r/{subreddit}/hot.json"
+    headers = {
+        "User-Agent": "Subreddit articles Viewer"
+    }
+    try:
+        r = requests.get(url, headers=headers)
+        if r.status_code == 200:
+            data = r.json()
+            item = data.get("data").get("children")
+            if i < len(item):
+                title = item[i].get("data").get("title")
+                if title not in hot_list:
+                    hot_list.append(title)
+                return recurse(subreddit, hot_list, i + 1)
+            else:
+                return hot_list
+            # print(title.get("data").get("title"))
+        else:
+            return None
+    except Exception:
+        pass
