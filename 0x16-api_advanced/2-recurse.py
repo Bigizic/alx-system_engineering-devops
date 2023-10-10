fc670fc4f5e995ee79da4d6702d07f93d8f0d83c
Bigiizc <olalekanisaac75@gmail.com>
2023-10-10 20:34:16 +0100

    Update recurse.py

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -11,27 +11,23 @@ import requests
 
 
 def recurse(subreddit, hot_list=[], i=0):
-    if subreddit:
-        url = f"https://www.reddit.com/r/{subreddit}/hot.json"
-        headers = {
-            "User-Agent": "Subreddit articles Viewer"
-        }
-        try:
-            response = requests.get(url, headers=headers)
-            if response.status_code == 200:
-                data = response.json()
-                item = data.get("data").get("children")
-                if i < len(item):
-                    title = item[i].get("data").get("title")
-                    if title not in hot_list:
-                        hot_list.append(title)
-                    return recurse(subreddit, hot_list, i + 1)
-                else:
-                    return hot_list
-                    # print(title.get("data").get("title"))
+    url = f"https://www.reddit.com/r/{subreddit}/hot.json"
+    headers = {
+        "User-Agent": "Subreddit articles Viewer"
+    }
+    try:
+        response = requests.get(url, headers=headers)
+        if response.status_code == 200:
+            data = response.json()
+            item = data.get("data").get("children")
+            if i < len(item):
+                title = item[i].get("data").get("title")
+                if title not in hot_list:
+                    hot_list.append(title)
+                return recurse(subreddit, hot_list, i + 1)
             else:
-                return None
-        except Exception:
+                return hot_list
+        else:
             return None
-    else:
+    except Exception:
         return None
