0b3dc9cc639be2f454600e0d564518400d74525f
Bigiizc <olalekanisaac75@gmail.com>
2023-10-11 17:41:04 +0100

    Update count.py

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -2,7 +2,7 @@
 """A python module that queries the Reddit API, parses the
 title of all hot articles, and prints a sorted count of
 given keywords (case-insensitive, delimited by spaces.
-Javascript should count as javascript, but java should not).
+Javascript should count as javascript, but java should not)
 """
 
 
@@ -15,8 +15,7 @@ def count_words(subreddit, word_list, hot_list=[], i=0, after=None):
         hot = 'hot'
         limit = 30
         url = "{}/r/{}/.json?sort={}&limit={}&count={}&after={}".format(
-                    reddit_url, subreddit, hot, limit, i,
-                    after if after else '')
+            reddit_url, subreddit, hot, limit, i, after if after else '')
 
         headers = {
             "User-Agent": "Subreddit articles Viewer"
@@ -29,36 +28,37 @@ def count_words(subreddit, word_list, hot_list=[], i=0, after=None):
             after = data.get("after")
             hot_list.extend(list(map(lambda x: x['data']['title'], item)))
             if after:
-                return count_words(subreddit, word_list, hot_list, i + count, after)
+                return count_words(subreddit, word_list, hot_list,
+                                   i + count, after)
             else:
                 return printer(hot_list, word_list, word_list)
         else:
             return
 
+
 def printer(hot_list, word_list, take, it=0, printed=[]):
     all_text = ' '.join(hot_list)
     all_words = all_text.lower().split()
-    if it < len(word_list):
-        word = word_list[0]
+    if it < len(take):
+        word = word_list[it]
         temp = all_words.count(word)
         if temp and word not in printed:
             printed.append({word: temp})
-            word_list.pop(0)
-            # print("{}: {}".format(word, temp))
+            # word_list.pop(0)
             return printer(hot_list, word_list, take, it + 1, printed)
         else:
             return printer(hot_list, word_list, take, it + 1, printed)
     else:
-        sorted_printed = sorted(printed, key=lambda item: (-list(item.values())[0], list(item.keys())[0]))
-        return output(sorted_printed, take)
+        sorted_printed = sorted(printed, key=lambda
+                                item: (-list(item.values())[0],
+                                       list(item.keys())[0]))
+        return output(sorted_printed)
 
-def output(printed, word_list, left=0):
-    i = len(word_list)
-    print(printed)
-    return
 
+def output(printed, left=0):
+    i = len(printed)
     if left < i:
         word = list(printed[left].keys())[0]
         count = list(printed[left].values())[0]
         print(f"{word}: {count}")
-        return output(printed, word_list, left + 1)
+        return output(printed, left + 1)
